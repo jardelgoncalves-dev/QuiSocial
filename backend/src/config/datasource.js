f978c0079e54b489e3debf79dead8ee262632aad
Jardel <jardelgoncalves1996@gmail.com>
2019-07-15 14:09:14 -0300

    add carregamento dinamico dos models

---
+++ backend/src/config/datasource.js
@@ -0,0 +1,40 @@
+import Sequelize from 'sequelize'
+import path from 'path'
+import fs from 'fs'
+import config from './config'
+
+let database = null
+
+const loadModels = (sequelize) => {
+  const dir = path.join(__dirname, '..', 'models')
+  let models = []
+  fs.readdirSync(dir).forEach(file => {
+    const modelDir = path.join(dir, file)
+    const model = sequelize.import(modelDir)
+    models[model.name] = model
+  })
+
+  return models
+}
+
+export default (app) => {
+  if (!database) {
+    const sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config.params
+    )
+
+    database = {
+      sequelize,
+      Sequelize,
+      models: {}
+    }
+
+    database.models = loadModels(sequelize)
+
+    sequelize.sync().done(() => database)
+  }
+  return database
+}
