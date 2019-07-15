d5185df605e8f719c5ee16850204aa655ebbb543
Jardel <jardelgoncalves1996@gmail.com>
2019-07-15 14:09:04 -0300

    add config do banco de dados

---
+++ backend/src/config/config.js
@@ -0,0 +1,13 @@
+export default {
+  database: 'db_quisocial',
+  username: 'postgres',
+  password: 'postgres',
+  params: {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
+    define: {
+      underscored: true
+    }
+  }
+}
