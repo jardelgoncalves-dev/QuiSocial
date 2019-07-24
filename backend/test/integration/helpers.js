dfb71b0e8289a93527905d0bbd36faf2da274aa5
Jardel <jardelgoncalves1996@gmail.com>
2019-07-23 22:14:39 -0300

    add helpers para testes de integração

---
+++ backend/test/integration/helpers.js
@@ -0,0 +1,11 @@
+import supertest from 'supertest'
+import { expect } from 'chai'
+import dotenv from 'dotenv'
+import app from '../../src/app'
+
+dotenv.config({
+  path: process.env.NODE_ENV !== 'test' ? '.env' : '.env.test'
+})
+
+global.request = supertest(app)
+global.expect = expect
