c4f74b009d862a53ef48316d66ef4ea3b467b3f3
Jardel <jardelgoncalves1996@gmail.com>
2019-07-24 00:44:12 -0300

    add primeiro test para a rota GET /users

---
+++ backend/test/integration/routes/users.test.js
@@ -0,0 +1,30 @@
+describe('Routes /users', () => {
+  const Users = app.datasource.models.Users
+  const userDefault = {
+    id: 1,
+    name: 'Fulano de Tal',
+    email: 'fulano@gmail.com',
+    password: 'test'
+  }
+
+  beforeEach(done => {
+    Users.destroy({ where: {} })
+      .then(() => Users.create(userDefault)
+        .then(() => {
+          done()
+        }))
+  })
+
+  describe('Route GET /users', () => {
+    it('should return a user lists', done => {
+      request
+        .get('/users')
+        .end((err, res) => {
+          expect(res.body[0].id).to.be.eql(userDefault.id)
+          expect(res.body[0].name).to.be.eql(userDefault.name)
+          expect(res.body[0].email).to.be.eql(userDefault.email)
+          done(err)
+        })
+    })
+  })
+})
