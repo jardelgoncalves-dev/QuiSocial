1404f03e80cca7e02352c7850dd01d5cfbd078e5
Jardel <jardelgoncalves1996@gmail.com>
2019-07-24 00:48:14 -0300

    add rota para GET /users/:id

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -5,8 +5,14 @@ export default (app) => {
   const _usersController = new UsersController(Users)
 
   app.route('/users')
-    .get( async (req, res) => {
+    .get(async (req, res) => {
       const result = await _usersController.getAll()
+      return res.status(result.status).json(result.data)
+    })
+  app.route('/users/:id')
+    .get(async (req, res) => {
+      const { id } = req.params
+      const result = await _usersController.getOne({ id })
       return res.status(result.status).json(result.data)
     })
 }
