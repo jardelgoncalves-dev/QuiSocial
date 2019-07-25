44ede95d403e516450ac2c7617c7b4e364fa8dea
Jardel <jardelgoncalves1996@gmail.com>
2019-07-25 20:13:52 -0300

    add rota para GET /posts/:id

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -9,4 +9,11 @@ export default (app) => {
       const result = await _postsController.getAll()
       return res.status(result.status).json(result.data)
     })
+
+  app.route('/posts/:id')
+    .get(async (req, res) => {
+      const { id } = req.params
+      const result = await _postsController.getOne({ id })
+      return res.status(result.status).json(result.data)
+    })
 }
