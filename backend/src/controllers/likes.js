f2eedc86f4d1fcb6fb76d48876a3f4de6c9de69b
Jardel <jardelgoncalves1996@gmail.com>
2019-07-26 11:30:05 -0300

    add controller likes

---
+++ backend/src/controllers/likes.js
@@ -0,0 +1,47 @@
+import Base from '../repository/base/repository-base'
+import Validators from '../helpers/validators'
+import { errorResponse } from '../helpers/response-message'
+
+export default class LikesController {
+  constructor (Model) {
+    this.Likes = new Base(Model)
+  }
+
+  getAll () {
+    return this.Likes.getAll()
+  }
+
+  getOne (params) {
+    return this.Likes.getOne(param)
+  }
+
+  create (data) {
+    const { postId } = data
+    const _validators = new Validators({
+      'postId.required': postId
+    })
+
+    if (_validators.hasError()) {
+      return errorResponse(_validators.errors)
+    }
+
+    return this.Likes.create(data)
+  }
+
+  update (params, data) {
+    const { postId } = data
+    const _validators = new Validators({
+      'postId.required': postId
+    })
+
+    if (_validators.hasError()) {
+      return errorResponse(_validators.errors)
+    }
+
+    return this.Likes.update(params, data)
+  }
+
+  delete (params) {
+    return this.Likes.delete(params)
+  }
+}
