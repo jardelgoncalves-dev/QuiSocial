8861644a27b59dfa4f36ca8ee04bfcd3bf16421d
Jardel <jardelgoncalves1996@gmail.com>
2019-07-25 23:05:15 -0300

    add middleware para validar autenticação nas rotas

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import { promisify } from 'util'
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provider!' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET)
+    req.userId = decoded.id
+    
+    return next()
+  } catch (error) {
+    return res.status(401).json({ error: 'Token invalid!' })    
+  }
+}
