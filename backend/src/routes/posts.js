8383104b6536eb515eba16429b518169330e2dbc
Jardel <jardelgoncalves1996@gmail.com>
2019-07-26 17:36:53 -0300

    add rota para GET /posts/user/:id

--- backend/src/routes/posts.js
+++ backend/src/routes/posts.js
@@ -3,12 +3,16 @@ import authMiddleware from '../middleware/auth'
 
 export default (app) => {
   const Posts = app.datasource.models.Posts
+  const Likes = app.datasource.models.Likes
+  const Users = app.datasource.models.Users
   const _postsController = new PostsController(Posts)
 
   app.route('/posts')
     .all(authMiddleware)
     .get(async (req, res) => {
-      const result = await _postsController.getAll()
+      const result = await _postsController.getAll([
+        { model: Likes }, { model: Users, attributes: { exclude: ['password'] }}
+      ])
       return res.status(result.status).json(result.data)
     })
     .post(async (req, res) => {
@@ -33,5 +37,13 @@ export default (app) => {
       const { id } = req.params
       const result = await _postsController.delete({ id })
       return res.status(result.status).send()
+    })
+  
+  app.route('/posts/user/:id')
+    .all(authMiddleware)
+    .get(async (req, res) => {
+      const { id } = req.params
+      const result = await _postsController.getAllByUser({ id }, [{ model: Likes }])
+      return res.status(result.status).json(result.data)
     })
 }
