77a89a5e0281af8ba6ec7dd3be567b9227905045
Jardel <jardelgoncalves1996@gmail.com>
2019-07-29 09:57:20 -0300

    encapsulando tudo no authConsumer

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -4,39 +4,37 @@ import { AuthConsumer } from '../../AppContext'
 import logo from '../../assets/images/logo.svg'
 
 export default () => (
-  <nav>
-    <AuthConsumer>
-      {({ isAuthenticated }) => (
-        <div className="navbar">
-          <input type="checkbox" id="navbar-check" />
-          <div className="navbar-header">
-            <div className="navbar-logo">
-              <img src={logo} alt="Logo do site" />
-            </div>
-            <div className="navbar-title">
-              <span>QuiSocial</span>
-            </div>
+  <AuthConsumer>
+    {({ isAuthenticated }) => (
+      <nav className="navbar">
+        <input type="checkbox" id="navbar-check" />
+        <div className="navbar-header">
+          <div className="navbar-logo">
+            <img src={logo} alt="Logo do site" />
           </div>
-          <div className="navbar-btn">
-            <label htmlFor="navbar-check">
-              <span />
-              <span />
-              <span />
-            </label>
+          <div className="navbar-title">
+            <span>QuiSocial</span>
           </div>
-          { isAuthenticated() ?(
-            <div className="navbar-links">
-              <Link to="#!">Home</Link>
-              <Link to="#!">Log out</Link>
-            </div>
-          ) : (
-            <div className="navbar-links">
-              <Link to="#!">Login</Link>
-              <Link to="#!">Cadastro</Link>
-            </div>
-          )}
         </div>
-      )}
-    </AuthConsumer>
-  </nav>
+        <div className="navbar-btn">
+          <label htmlFor="navbar-check">
+            <span />
+            <span />
+            <span />
+          </label>
+        </div>
+        { isAuthenticated() ?(
+          <div className="navbar-links">
+            <Link to="/home">Home</Link>
+            <Link to="/">Log out</Link>
+          </div>
+        ) : (
+          <div className="navbar-links">
+            <Link to="/">Login</Link>
+            <Link to="/cadastro">Cadastro</Link>
+          </div>
+        )}
+      </nav>
+    )}
+  </AuthConsumer>
 )
