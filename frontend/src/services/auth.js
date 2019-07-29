9cfe76ac2d1ad61d1b28a97f29c0ad73dfd95291
Jardel <jardelgoncalves1996@gmail.com>
2019-07-29 16:59:10 -0300

    reduzindo a quantidade de metodos

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -2,20 +2,20 @@ export const TOKEN_KEY = '@QuiSocial/token'
 export const USER_AUTH = '@QuiSocial/user'
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null
 export const getToken = () => localStorage.getItem(TOKEN_KEY)
-export const login = token => {
+
+export const login = (token, user) => {
   localStorage.setItem(TOKEN_KEY, token)
+  localStorage.setItem(USER_AUTH, JSON.stringify({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    bio: user.bio,
+    photoName: user.photoName
+  }))
 }
 export const logout = () => {
   localStorage.removeItem(TOKEN_KEY)
   localStorage.removeItem(USER_AUTH)
 }
 
-export const storeUser = user => {
-  localStorage.setItem(USER_AUTH, JSON.stringify({
-    id: user.id,
-    name: user.name,
-    email: user.email
-  }))
-}
-
-export const getUser = () => isAuthenticated() ? JSON.parse(localStorage.getItem(USER_AUTH)) : null
+export const getUser = () =>  JSON.parse(localStorage.getItem(USER_AUTH))
