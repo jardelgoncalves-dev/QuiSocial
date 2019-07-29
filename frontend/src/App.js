668df800e67ce4fbe402acdb6d5456a50c24e0af
Jardel <jardelgoncalves1996@gmail.com>
2019-07-29 00:37:09 -0300

    atualizando arquivo app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
+import Routes from './routes'
+import './assets/css/style.css'
 
-const App = () => (
-  <h1>Hello World</h1>
-)
+const App = () => <Routes />
 
 export default App;
