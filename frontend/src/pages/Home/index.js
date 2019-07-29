f318a529009e4f50e0e3657db5bc6c9a8b1375cd
Jardel <jardelgoncalves1996@gmail.com>
2019-07-29 16:59:44 -0300

    add card user e card menu do usuario

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
+
+import NavBar from '../../components/NavBar'
+import UserCard from '../../components/Card/UserInfo'
+import Container from '../../components/Container'
+import Row from '../../components/Row'
+import Col from '../../components/Col'
 
 class Home extends Component {
   render () {
     return (
-      <h1>Hello</h1>
+      <div>
+        <NavBar />
+        <Container>
+          <Row>
+            <Col className='md-4 lg-3'>
+              <Row>
+                <Col className='col-12 md-12 lg-12'>
+                  <UserCard />
+                </Col>
+                <Col className="col-12 sm-6 md-12 lg-12">
+                  <div className="card">
+                    <div className="menu-options">
+                      <ul>
+                        <li><Link href="/perfil"><i className="far fa-user"></i>Meu perfil</Link></li>
+                        <li><Link href="/editar"><i className="fas fa-pen"></i>Editar perfil</Link></li>
+                      </ul>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   } 
 }
