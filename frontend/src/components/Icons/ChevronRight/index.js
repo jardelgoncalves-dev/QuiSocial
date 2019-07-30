1da943a76ced0775e53d0c63521b8ec898b87799
Jardel <jardelgoncalves1996@gmail.com>
2019-07-30 13:53:04 -0300

    add Icons components

---
+++ frontend/src/components/Icons/ChevronRight/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default ({ fill, width, height }) => (
+  <svg viewBox="0 0 24 24" width={width || 16} height={height || 16} fill={fill || '#5DADE2'}>
+    <path d="M 7.5346604,0.91525424 4.9416949,3.5082204 13.364236,11.949152 4.9416949,20.390084 7.5346604,22.983051 18.568559,11.949152 Z" id="path4" />
+    <path d="M0 0h24v24H0z" fill="none" id="path6" />
+  </svg>
+)
